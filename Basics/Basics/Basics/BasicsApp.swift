e10e0ded2cf3bcf774124bc51fd776ecf95cd8b3
glbrom <golbrm@gmail.com>
2024-09-23 16:59:49 +0300

    Created Basics prjct

---
+++ Basics/Basics/Basics/BasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicsApp.swift
+//  Basics
+//
+//  Created by Roman Golub on 23.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct BasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
